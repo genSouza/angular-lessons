d0fa1872443591940e976127b10798a94a114651
Genilton Souza <genilton.souza26@gmail.com>
2020-12-16 00:02:57 -0400

    lesson 3
    Hello, World! Criando primeiro projeto e o primeiro Componente

---
+++ first-project/src/app/my-first/my-first.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-first-component',
+  template: `<p>My first component with Angular 2</p>`,
+})
+export class MyFirstComponent {}
---
+++ first-project/src/app/my-second/my-second.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-second-component',
+  templateUrl: './my-second.component.html',
+  styleUrls: ['./my-second.component.css']
+})
+export class MySecondComponent {}
+
