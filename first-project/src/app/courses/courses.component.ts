781dac688ae73655f50b22c7db6c42f1388ed35f
Genilton Souza <genilton.souza26@gmail.com>
2020-12-16 09:28:18 -0400

    Lesson 6
    Templates

--- first-project/src/app/courses/courses.component.ts
+++ first-project/src/app/courses/courses.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'courses-component',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.css']
+  styleUrls: ['./courses.component.css'],
 })
 export class CoursesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  portalName: string;
+  courses: string[] = ['java', 'Ext js', 'Angular'];
+  constructor() {
+    this.portalName = 'http://loiane.trainnig';
   }
 
+  ngOnInit(): void {}
 }
